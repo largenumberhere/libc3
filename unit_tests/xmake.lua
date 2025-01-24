c33b8104beceb11f7ae2aee344a38808b51a1df9
largenumberhere <largenumbergoeshere@gmail.com>
2025-01-24 17:28:30 +1100

    working on xmake script

--- unit_tests/xmake.lua
+++
@@ -1,8 +0,0 @@
-includes("../libc3")
-
-target("strlen1")
-    set_kind("binary")
-    add_files("strlen1.cpp")
-    add_deps("libc3")
-    -- add_headerfiles("../libc3/include/mangled.hpp")
-    
--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,29 @@
 add_plugindirs("xmake_plugins")
 
-includes("./unit_tests")
-includes("./libc3")
 
-target("all")
+target("libc3")    
+    on_build(function(target) 
+        if target:is_arch("x86_64") and target:is_plat("linux") then
+            target:add("files", "./libc3/src_platform/x86_64-gnu-linux/*.cpp")
+        else 
+        error("unsupported platform")
+        end
+    end)
+    add_cxxflags("gcc::-ffreestanding -nostdlib -nostartfiles")
     set_kind("static")
-    add_deps("unit_tests")
-
-
-target("unit_tests")
-    set_kind("static")
-    add_headerfiles("libc3/include/mangled.hpp")
+    add_files("libc3/src/*/*.cpp")
+    set_languages("cxx23")
+    add_headerfiles("./libc3/include/mangled.hpp")
+    add_headerfiles("./libc3/include/unmangled.hpp")
     
+
+target("tests")
+    add_deps("libc3")
+    set_kind("binary")
+    set_default(false)
+    add_headerfiles()
+    
+    add_tests("./unit_tests/*.cpp")
+    
+    
+
