c7df6a3a92922f4674313732b1ed2d786d9f1115
largenumberhere <largenumbergoeshere@gmail.com>
2025-02-12 03:38:00 +1100

    working on automated test detection script

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,8 @@
 add_plugindirs("xmake_plugins")
 set_policy("check.auto_ignore_flags", false)
 
+
+
 function setupTest()
     add_deps("libc3")
     set_kind("binary")
@@ -9,6 +11,43 @@ function setupTest()
     add_cxxflags("-g")
     set_default(false)
 end
+
+-- on_config(registerTests)
+
+-- local function registerTests()
+--     local endswith = function(str, suffix) 
+--         return string.sub(str, -#suffix) == suffix 
+--     end
+    
+--     local lines = function(str) 
+--         return string.gmatch(str, "[^\r\n]+")
+--     end
+    
+--     local function createMangledUnitTest(name) 
+--         target(name)
+--             setupTest()
+--             add_files("./unit_tests/"..name..".c" )
+--             add_tests(name, {pass_outputs= "ok"})
+--             add_headerfiles("./libc3/include/mangled.hpp")
+--     end
+        
+
+--     root_path = "./unit_tests/"
+--     mangled_suffix = "a.c"
+    
+--     -- get source files
+--     files = os.iorunv("find", {root_path})
+--     print("found" .. tostring(files))
+--     for file_path in lines(files) do 
+--         if (endswith(file_path, mangled_suffix)) then
+--             without_file_extension = string.sub(file_path, 0, -3)
+--             createMangledUnitTest(without_file_extension)
+--         else
+--             print("warning: b tests are unimplemented")
+--         end
+
+--     end
+-- end
 
 target("libc3")   
 
@@ -29,6 +68,9 @@ target("libc3")
     add_cflags("-g")
     add_cxxflags("-g")
 
+
+
+
 -- test with standard library
 target("strlen1a")
     setupTest()
@@ -38,16 +80,6 @@ target("strlen1a")
     add_headerfiles("./libc3/include/mangled.hpp")
     -- add_ldflags("-stdlib=libstdc++", {force = true})
     
--- -- test without standard library
--- target("strlen1b")
---     setupTest()
---     add_headerfiles("./libc3/include/unmangled.hpp") 
---     add_ldflags("-nostartfiles", "-ffreestanding", "-nostdlib")
---     add_cflags("-ffreestanding", "-nostdlib")
---     add_files("./unit_tests/strlen1b.c")
-
---     add_tests("strlen1b")
---     add_tests("strlen1b", {pass_outputs="ok"})
 
 target("fgetc1a")
     setupTest()
@@ -78,3 +110,15 @@ target("strncmp1a")
     add_files("./unit_tests/strncmp1a.c")
     add_tests("strncmp1a", {pass_outputs="ok"})
     add_headerfiles("./libc3/include/mangled.hpp")
+
+
+-- -- test without standard library
+-- target("strlen1b")
+--     setupTest()
+--     add_headerfiles("./libc3/include/unmangled.hpp") 
+--     add_ldflags("-nostartfiles", "-ffreestanding", "-nostdlib")
+--     add_cflags("-ffreestanding", "-nostdlib")
+--     add_files("./unit_tests/strlen1b.c")
+
+--     add_tests("strlen1b")
+--     add_tests("strlen1b", {pass_outputs="ok"})
